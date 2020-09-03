e80ec8f8527e9c0a4c9618d2b20b1685313e7468
aramic1989 <63340759+aramic1989@users.noreply.github.com>
2020-09-03 18:27:44 -0500

    final queries

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 ## Part 1: Test it with SQL
-
+SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMN WHERE TABLE_SCHEMA = 'techjobs' AND TABLE_NAME = 'job';
 ## Part 2: Test it with SQL
-
+SELECT * FROM techjobs.employer WHere location = "St. Louis City";
 ## Part 3: Test it with SQL
-
+DROP TABLE job;
 ## Part 4: Test it with SQL
+SELECT DISTINCT skill.name, skill.description FROM skill INNER JOIN  job_skills ON skill.id = job_skills.skills_id
+ORDER BY skills.name;
