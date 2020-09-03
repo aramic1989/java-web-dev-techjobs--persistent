d1bfa453212519737b1e905401b1d0f8131498fd
aramic1989 <63340759+aramic1989@users.noreply.github.com>
2020-09-03 18:43:02 -0500

    pt 3 change

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,28 @@
-package org.launchcode.javawebdevtechjobspersistent.models;
-
+ppackage org.launchcode.javawebdevtechjobspersistent.models;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
 public class Employer extends AbstractEntity {
-
-
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Please enter a location")
+    private String location;
+    public Employer() {
+    }
+    @OneToMany(mappedBy = "employer", cascade = CascadeType.ALL)
+    private final List<Job> jobs = new ArrayList<>();
+    //Getters and Setters
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
