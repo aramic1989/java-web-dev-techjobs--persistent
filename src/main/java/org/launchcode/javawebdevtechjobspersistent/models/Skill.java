db959ba13045cd584fd12723752cbb93c6839e9e
aramic1989 <63340759+aramic1989@users.noreply.github.com>
2020-09-03 18:45:47 -0500

    pt 3 and 4 done

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,34 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 6)
+    private String description;
+
+    public Skill(){
+    }
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+    //getters and setters//
+  
+    public List<Job> getJobs() {
+        return jobs;
+    }
+    public Skill(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
